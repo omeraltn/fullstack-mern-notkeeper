b7ce46c6fac6141e29b8fa52918d150a6a8cb2dd
omeraltn <omer.arc.altun56@gmail.com>
2026-06-15 16:39:11 +0300

    project update

--- front-end/src/hooks/useLogout.jsx
+++ front-end/src/hooks/useLogout.jsx
@@ -1,10 +1,13 @@
 import { useAuthContext } from "./useAuthContext";
+import { useNotContext } from "./useNotContext";
 
 export const useLogout = () => {
   const { dispatch } = useAuthContext();
+  const { dispatch: notDispatch } = useNotContext();
 
   const logout = () => {
     localStorage.removeItem("Kullanici");
+    notDispatch({ type: "NOT_DOLDUR", payload: null });
     dispatch({ type: "LOGOUT" });
   };
 
